e51782a2ef9b387cb7e031dbedc9205f1711d108
noodles4u <louisdesmett@gmail.com>
2023-06-02 12:43:37 +0200

    going to test something other than readline

--- manager.js
+++ manager.js
@@ -8,36 +8,69 @@ var rl = readline.createInterface(process.stdin, process.stdout);
 let menu =
   'Welcome to your task manager,Press:\n 1. to see all your tasks\n 2. to add a task\n 3. to delete a task\n 4. to mark a task as done\n 5. to Exit the task manager';
 
-let tasks = ['Wash the dishes', 'Fold the laundry'];
+let tasks = [
+  {
+    item: 'Empty Dishwasher',
+    status: 'unfinished',
+  },
+  {
+    item: 'Do the laundry',
+    status: 'unfinished',
+  },
+];
 
 const showtasks = () => {
-  console.log(menu);
+  return menu;
 };
+showtasks();
 
-rl.question(showtasks(), (number) => {
-  switch (parseInt(number)) {
-    case 1:
-      console.log(tasks);
-      break;
+// Adding task to array
+const addTask = () => {
+  rl.question(console.log('New task?'), (newItem) => {
+    tasks.push({ item: `${newItem}`, status: 'unfinished' });
 
-    case 2:
-      console.log('number 2');
-      break;
+    rl.close();
 
-    case 3:
-      console.log('number 3');
-      break;
+    return tasks;
+  });
+};
+addTask();
 
-    case 4:
-      console.log('number 4');
-      break;
+// rl.question(console.log(showtasks()), (number) => {
+//   switch (parseInt(number)) {
+//     case 1:
+//       console.log(tasks);
+//       break;
 
-    case 5:
-      console.log('number 5');
-      break;
-    default:
-      console.log('Wrong number pal, try again!');
-  }
+//     case 2:
+//       console.log(addTask());
+//       break;
 
-  rl.close();
-});
+//     case 3:
+//       console.log('number 3');
+//       break;
+
+//     case 4:
+//       console.log('number 4');
+//       break;
+
+//     case 5:
+//       console.log('number 5');
+//       break;
+//     default:
+//       console.log('Wrong number pal, try again!');
+//   }
+
+//   rl.close();
+// });
+
+// let tasksProperties = tasks.map((newTask) => {
+//   let Properties = {
+//     task: tasks.item,
+//     status: 'Unfinished',
+//   };
+//   tasks.item.push(newTask);
+//   return;
+// });
+
+// tasksProperties();
